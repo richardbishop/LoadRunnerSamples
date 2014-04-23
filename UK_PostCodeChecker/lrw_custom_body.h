4ce836ef432d18bcce0e39465dd16fe57f414058
richardmjbishop <richard@bish.co.uk>
2014-04-23 16:52:20 +0100

    Removed duplicate script

--- UK_PostCodeChecker/lrw_custom_body.h
+++
@@ -1,5 +0,0 @@
-/*********************************************************
-// This file contains the body sections 
-// recorded for web_custom_request function.
-**********************************************************/
-
--- UK_PostCodeChecker/vuser_init.c
+++
@@ -1,4 +0,0 @@
-vuser_init()
-{
-	return 0;
-}
