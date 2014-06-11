ee7566ff69a701858bd848a689ba6948b48a4e17
richardmjbishop <richard@bish.co.uk>
2014-06-11 15:24:02 +0100

    RandomAddressGenerator Script added
    Added a TruClientscript which calls
    http://www.doogal.co.uk/RandomAddresses.php, obtains a random address
    and saves it to a parameter file

---
+++ RandomAddressGenerator/C-functions.c
@@ -0,0 +1,11 @@
+//
+// To view/edit the script, click the Develop Script button
+//
+// This file defines the data and functions that will be used by EvalC
+// steps in the script
+//
+// Note: this file is mandatory, do not delete it.
+void main()
+{
+	
+}
